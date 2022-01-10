5c9450964f3b505181086d0b38b59c073ba9c19c
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-10 19:36:03 +0900

    Completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    split_word = @word.split('')
+    found_word = ''
+    found_word =
+      array.filter do |word|
+        found_word = word.split('').sort == split_word.sort
+      end
+    found_word
+  end
+  # binding.pry
+end
